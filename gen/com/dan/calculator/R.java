f0e21652491f27a980b9c63031b6707c073bc3c7
DanWimpress <daniel.wimpress@gmail.com>
2014-07-24 20:38:26 +0100

    Started work on layout

--- gen/com/dan/calculator/R.java
+++ gen/com/dan/calculator/R.java
@@ -23,9 +23,27 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int verticle_divider=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080011;
+        public static final int add=0x7f080010;
+        public static final int clear=0x7f08000d;
+        public static final int display=0x7f080000;
+        public static final int divide=0x7f080004;
+        public static final int eight=0x7f080002;
+        public static final int equal=0x7f08000f;
+        public static final int five=0x7f080006;
+        public static final int four=0x7f080005;
+        public static final int multiply=0x7f080008;
+        public static final int nine=0x7f080003;
+        public static final int one=0x7f080009;
+        public static final int seven=0x7f080001;
+        public static final int six=0x7f080007;
+        public static final int subtract=0x7f08000c;
+        public static final int three=0x7f08000b;
+        public static final int two=0x7f08000a;
+        public static final int zero=0x7f08000e;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -35,8 +53,25 @@ public final class R {
     }
     public static final class string {
         public static final int action_settings=0x7f050002;
+        public static final int add=0x7f050011;
         public static final int app_name=0x7f050000;
+        public static final int clear=0x7f050013;
+        public static final int display=0x7f050003;
+        public static final int divide=0x7f05000e;
+        public static final int eight=0x7f05000c;
+        public static final int equal=0x7f050012;
+        public static final int five=0x7f050009;
+        public static final int four=0x7f050008;
         public static final int hello_world=0x7f050001;
+        public static final int multiply=0x7f05000f;
+        public static final int nine=0x7f05000d;
+        public static final int one=0x7f050005;
+        public static final int seven=0x7f05000b;
+        public static final int six=0x7f05000a;
+        public static final int subtract=0x7f050010;
+        public static final int three=0x7f050007;
+        public static final int two=0x7f050006;
+        public static final int zero=0x7f050004;
     }
     public static final class style {
         /** 
